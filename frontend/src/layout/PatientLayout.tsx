b2fe6fb4099277b061265d9e5755afe0b782eb26
albert <yiyevp@gmail.com>
2025-10-04 15:49:14 -0300

    WIP - Refactorizar PatientLayout para eliminar elementos de menú no utilizados y limpiar las importaciones.

--- frontend/src/layout/PatientLayout.tsx
+++ frontend/src/layout/PatientLayout.tsx
@@ -1,4 +1,4 @@
-import { Inbox, LayoutDashboard, Settings, Users } from "lucide-react";
+import { Inbox, LayoutDashboard } from "lucide-react";
 import { Link } from "react-router";
 import logo from "@/assets/images/logos/logo-black.png";
 import { NavUser } from "@/components/nav.user";
@@ -38,18 +38,6 @@ export default function PatientLayout({ children }: Props) {
 			url: "/pacientes",
 			icon: Inbox,
 			roles: ["admin", "planner"] as UserRole[],
-		},
-		{
-			title: "Planificadores",
-			url: "/planificadores",
-			icon: Users,
-			roles: ["admin"] as UserRole[],
-		},
-		{
-			title: "Accesos",
-			url: "/accesos",
-			icon: Settings,
-			roles: ["admin"] as UserRole[],
 		},
 	];
 
